5feb084330c139e6f593871919961005b4b5c0d1
pankajoberoi <poberoi1557.ca19@chitkara.edu.in>
2024-12-08 08:48:32 +0530

    updated

---
+++ Js/fundamentals.js
@@ -0,0 +1,111 @@
+
+
+// console.log("Hello world")//output
+
+
+// console.log("Jai shree ram")
+
+// let var const=========================================
+
+// let firstName="Pankaj";
+
+// console.log(firstName)
+
+// firstName = "Jhumruu"
+// console.log(firstName)
+
+
+// const PI = 3.14;
+
+// PI=2.13;
+
+// console.log(PI);
+
+// let last_Name="Oberoi"
+// console.log(last_Name);
+
+//Data TYPES================================================
+// 1.Numbers
+// 2.Strings
+// 3.Boolean
+// 4.undefined
+// 5.NULL
+
+
+// let name1;
+// console.log(name1);
+
+
+// let x=10;
+// console.log("x ki value in number" + x);
+
+// x="Pankaj"
+// console.log("X ki value in string " + x);
+
+
+
+// ==============================================================
+// typeof
+
+// console.log(typeof(true))
+// console.log(typeof("Kalpana"))
+// console.log(typeof(23))
+// console.log(typeof(21.2))
+// console.log(typeof(undefined))
+// console.log(typeof(null))
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Basic Operators
+
+// Mathematical operator => +,-,*,/,%
+//assignement  operator =>  =, +=,-=,*=,/=,%=
+ //comparison => ==,===,>=,<=,!=
+
+// console.log(4/2);questionent
+
+// console.log(5%2);remainder
+
+
+// let a=5;
+
+// let b=10;
+
+// b+=a;
+
+// b=b+a;
+
+// console.log(b);
+
+
+// console.log("5" === 5);
+
+
+// let currentYear =2024
+
+// console.log(currentYear+=10 < (currentYear+=25))
+//operator precedence
+// true -> lakshmi,vishal,soumya
+// false -> shaheen,kalpana
+// 2025 -> vijay,akhilesh
+
+
+
+// ===============================================
+// Strings , string templates /template litrals
+
+
+const name="Pankaj"
+const job="Trainer"
+const birthyear=2000
+const year=2024;
+
+
+
+// const profile= "I am " + name + " my job is " + job + " i am " + (2024-2000) + " years old"
+
+// console.log(profile)
+
+// const profile = `I am ${name} my job is ${job} i am ${year-birthyear} years old`
+
+// console.log(profile)
+
