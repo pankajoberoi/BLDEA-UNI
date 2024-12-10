48d75ae8a1e4f3cf024b9f487265e4e139edba27
pankajoberoi <poberoi1557.ca19@chitkara.edu.in>
2024-12-10 11:43:40 +0530

    updated

--- quizApp/quiz.js
+++ quizApp/quiz.js
@@ -37,3 +37,112 @@ const questions=[
         ]
     }
 ];
+
+
+let questionElement=document.getElementById("question")
+let answerButton=document.getElementById("answer-buttons")
+let nextButton= document.getElementById("next-btn")
+
+let currentQuestionIndex;
+let score;
+
+function startQuiz(){
+    currentQuestionIndex=0
+    score=0
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion=questions[currentQuestionIndex];
+    let questionNo= currentQuestionIndex + 1;
+
+    questionElement.innerHTML = questionNo + " . " +  currentQuestion.question;
+
+    
+    currentQuestion.answers.forEach((answer) => {
+        const button=document.createElement("button"); 
+        button.innerHTML=answer.text;
+        button.classList.add("btn")
+        answerButton.appendChild(button);
+
+        if(answer.correct) {
+            button.dataset.correct=answer.correct;
+        }       
+
+        button.addEventListener("click",selectAnswer)
+
+    })
+
+  
+
+}
+
+
+function resetState(){
+    nextButton.style.display="none"
+    while(answerButton.firstChild){
+        answerButton.removeChild(answerButton.firstChild)
+    }
+
+}
+
+function selectAnswer (event){
+    console.log(event)//object of clicked button
+    const selectedBtn= event.target;
+    const isCorrect=selectedBtn.dataset.correct==="true"
+
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect");
+    }
+    
+ 
+
+    Array.from(answerButton.children).forEach((button)=>{
+        if(button.dataset.correct==="true"){
+            button.classList.add("correct")
+        }
+
+        button.disabled=true
+
+    })    
+    
+    nextButton.style.display="block"
+    
+
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML=`Your Score is ${score}`
+    nextButton.innerHTML="Play Again"
+    nextButton.style.display="block"
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex<questions.length){
+        showQuestion();
+    }
+    else{
+        showScore();
+    }
+}
+
+
+
+nextButton.addEventListener("click",()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }
+    else{
+        startQuiz();
+    }
+})
+
+
+startQuiz();
